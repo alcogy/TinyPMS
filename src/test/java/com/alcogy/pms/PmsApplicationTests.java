500c91c64da19f7bf8545185e4f4dc780ea35247
nasm <info@alcogy.com>
2025-04-14 10:53:08 +0900

    Update settings and ui.

--- src/test/java/com/alcogy/pms/PmsApplicationTests.java
+++ src/test/java/com/alcogy/pms/PmsApplicationTests.java
@@ -1,13 +1,8 @@
 package com.alcogy.pms;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PmsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
